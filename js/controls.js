8b197dcfd3c11c014379a02399c36167c5104462
Don Burks <don@donburks.com>
2016-08-21 22:46:53 -0500

    Fixing SVG saving, adding bgcolor and linecolor params

--- js/controls.js
+++ js/controls.js
@@ -1,9 +1,24 @@
 $(function() {
+  defaultParams.bgcolor = "#ffffff";
+  defaultParams.linecolor = "#000000";
+
   function addSVG(div) {
-      return div.insert("svg", ":first-child")
-          .attr("height", 400)
-          .attr("width", 400)
-          .attr("viewBox", "-500 -500 1000 1000");
+    return div.insert("svg", ":first-child")
+      .attr("height", 400)
+      .attr("width", 400)
+      .attr("viewBox", "-500 -500 1000 1000");
+  }
+
+  function lpad(num) {
+    return ("00"+num).slice(-2);
+  }
+
+  function genDate() {
+    var date = new Date();
+    var string = date.getFullYear()+"";
+    string += lpad((date.getMonth() + 1));
+    string += lpad(date.getDate());
+    return string;
   }
 
   var finalDiv = d3.select("div#final");
@@ -12,8 +27,17 @@ $(function() {
   finalDiv.append("button")
       .text("Generate high resolution map")
       .on("click", function () {
-          doMap(finalSVG, defaultParams);
+        doMap(finalSVG, defaultParams);
+        finalDiv.append("button")
+          .text("Save to PNG")
+          .on("click", function () {
+              var ds = genDate();
+              var svg = $("#final").find('svg');
+              console.log(svg);
+              saveSvgAsPng(svg[0], "map-"+ds + ".png", {backgroundColor: defaultParams.bgcolor, top: -500, left: -500});
+          });
       });
+
 
   $("legend").on('click', function() {
     var fs = $(this).parents('fieldset');
@@ -23,6 +47,15 @@ $(function() {
     } else {
       fs.addClass('closed');
       $(this).find('span').text('+');
+    }
+  });
+
+  $(".controls").find('input').on('change', function() {
+    var field = $(this).data('field'); 
+    defaultParams[field] = $(this).val();
+    if (/color$/.test(field)) {
+      $("svg").css('background-color', defaultParams.bgcolor);
+      $("path, line").css('stroke', defaultParams.linecolor);
     }
   });
 });
